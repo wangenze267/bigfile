c01e8f4d93ce3a3c9aa22c60f83a152737e3b89e
wangenze267 <15041567431@126.com>
2024-03-16 15:40:34 +0800

    feat 拖拽预览

--- client/src/FileUploader.js
+++ client/src/FileUploader.js
@@ -4,12 +4,30 @@ import './FileUploader.css';
 import useDrag from './useDrag.js'
 function FileUploader() {
   const uploadContainerRef = useRef(null)
-  useDrag(uploadContainerRef)
+  const { selectedFile, filePreview } = useDrag(uploadContainerRef)
   return (
-    <div className='uploader-container' ref={ uploadContainerRef }>
-      <InboxOutlined/>
-    </div>
+    <>
+      <div className='uploader-container' ref={ uploadContainerRef }>
+        {renderFilePreview(filePreview)}
+      </div>
+    </>
   )
+}
+// 显示文件预览
+function renderFilePreview(filePreview) {
+  const { url, type} = filePreview
+  if(url) {
+    console.log(url)
+    if (type.startsWith('image/')) {
+      return <img src={ url } alt="preview"></img>
+    } else if (type.startsWith('video/')) {
+      return <video src={ url } alt="preview"></video>
+    } else {
+      return url
+    }
+  } else {
+    return <InboxOutlined />
+  }
 }
 
 export default FileUploader;
--- client/src/useDrag.js
+++ client/src/useDrag.js
@@ -1,16 +1,50 @@
 import { useState,useEffect, useCallback } from 'react'
+import { message } from 'antd'
+import { MAX_FILE_SIZE } from './constant'
 
 function useDrag(uploadContainerRef) {
+  // 定义一个状态用来保存用户选中的文件
+  const [ selectedFile, setSelectedFile ] = useState(null)
+  // 存放文件预览信息 url 是预览地址 type 是文件类型
+  const [filePreview, setFilePreview] = useState({ url: null, type: null })
   const handleDrag = useCallback((event) => {
     event.preventDefault() // 阻止默认行为
     event.stopPropagation() // 阻止事件传播
   },[])
+  const checkFile = (files) => {
+    const file = files[0]
+    if (!file) {
+      message.error('没有选择任何文件')
+      return
+    }
+    // 文件大小不能超过2G
+    if (file.size > MAX_FILE_SIZE) {
+      message.error('大小不能超过2G！')
+      return
+    }
+    // 判断文件类型 限制图片和视频
+    if (!(file.type.startsWith('image/') || file.type.startsWith('video/'))) {
+      message.error('文件类型必须是图片或视频文件！')
+      return
+    }
+    setSelectedFile(file)
+  }
   const handleDrop = useCallback((event) => {
     event.preventDefault() // 阻止默认行为
     event.stopPropagation() // 阻止事件传播
-    const {files} = event.dataTransfer
-    console.log(files)
+    checkFile(event.dataTransfer.files)
   },[])
+  useEffect(() => {
+    if(!selectedFile) {
+      return
+    }
+    const url = URL.createObjectURL(selectedFile)
+    setFilePreview({ url, type:selectedFile.type })
+    // useEffect 会返回一个销毁函数
+    return () => {
+      URL.revokeObjectURL(url)
+    }
+  },[selectedFile])
   // useCallback 如果不给第二个参数 是没有缓存效果的 每一次都是新的
   useEffect(() => {
     const uploadContainer = uploadContainerRef.current
@@ -25,6 +59,6 @@ function useDrag(uploadContainerRef) {
       uploadContainer.removeEventListener('dragleave', handleDrag)
     }
   },[])
-  return {}
+  return { selectedFile, filePreview }
 }
 export default useDrag
